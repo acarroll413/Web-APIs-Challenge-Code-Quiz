ad8f7812701fc0ec0d539f49cd5acd0b7905a3f3
unknown <acarroll0413@gmail.com>
2021-12-03 20:54:34 -0600

    second commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const startButton = document.getElementById("start-btn")
+
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+    console.log("started")
+}
+
+function setNextQuestion() {
+
+}
